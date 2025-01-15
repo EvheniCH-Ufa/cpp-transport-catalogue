c56dfd41dba0f150309ad23746b4950ff465ca43
EvheniCH-Ufa <Ch.Ew.Se@yandex.ru>
2025-01-15 22:15:09 +0500

    Update main.cpp
    Review_8_2

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -11,24 +11,6 @@ using namespace std;
 int main() {
     Transport::Data::TransportCatalogue catalogue;
 
-    int base_request_count;
-    cin >> base_request_count >> ws;
-
-    {
-        Transport::InputCataloge::InputReader reader;
-        for (int i = 0; i < base_request_count; ++i) {
-            string line;
-            getline(cin, line);
-            reader.ParseLine(line);
-        }
-        reader.ApplyCommands(catalogue);
-    }
-
-    int stat_request_count;
-    cin >> stat_request_count >> ws;
-    for (int i = 0; i < stat_request_count; ++i) {
-        string line;
-        getline(cin, line);
-        Transport::PrintCataloge::ParseAndPrintStat(catalogue, line, cout);
-    }
+    Transport::InputCataloge::Test(catalogue);
+    Transport::PrintCataloge::Test(catalogue);
 }
