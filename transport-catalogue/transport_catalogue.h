7d7cbd07038070400e326b6f85367eaf2f11d1d7
EvheniCH-Ufa <Ch.Ew.Se@yandex.ru>
2025-01-15 22:18:11 +0500

    Update transport_catalogue.h
    Review_8_2

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -5,7 +5,11 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <set>
+
 #include "geo.h"
+
+
 
 namespace Transport
 {
@@ -17,8 +21,8 @@ namespace Transport
 		{
 			std::string name;
 			Coordinates coordinates;
-			const std::vector<Bus*>& GetBuses() const;
-			std::vector<Bus*> buses = {};
+			const std::set<std::string>& GetBuses() const;
+			std::set<std::string> buses = {};
 		};
 
 		struct Bus
@@ -29,11 +33,12 @@ namespace Transport
 			size_t unique_stop_count;
 		};
 
+
 		class TransportCatalogue
 		{
 			// Реализуйте класс самостоятельно
 		public:
-			void AddBus(const std::string& bus_name, std::vector<std::string_view>& stops_names);
+			void AddBus(const std::string& bus_name, const std::vector<std::string_view>& stops_names);
 			void AddStop(const std::string& stop_name, Coordinates coordinates); //name + shirota + dolgota
 			const Bus* GetBus(std::string_view bus_name) const;
 			const Stop* GetStop(std::string_view stop_name) const;
@@ -42,8 +47,8 @@ namespace Transport
 			std::deque<Bus> buses_;
 			std::deque<Stop> stops_;
 
-			std::unordered_map <std::string*, Bus*> buses_for_find_;
-			std::unordered_map <std::string*, Stop*> stops_for_find_;
+			std::unordered_map <std::string_view, Bus*> buses_for_find_;
+			std::unordered_map <std::string_view, Stop*> stops_for_find_;
 		};
 	}
 }
