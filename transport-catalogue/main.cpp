1aeac783a4bb6cd1aada059c7cfbf8fab3c0b702
EvheniCH-Ufa <Ch.Ew.Se@yandex.ru>
2025-01-15 23:44:43 +0500

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,10 +1,10 @@
 // p08_02_08_TransportnyjSpravochnik_PishemKod_3.cpp
 
 #include <iostream>
-#include <string>
 
 #include "input_reader.h"
 #include "stat_reader.h"
+#include "transport_catalogue.h"
 
 int main() {
     Transport::Data::TransportCatalogue catalogue;
