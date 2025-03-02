7d67d3e29cf59ece1ad2c6734a842882a0f968ff
EvheniCH-Ufa <Ch.Ew.Se@yandex.ru>
2025-03-02 16:51:01 +0500

    Update transport_catalogue.h
    Code Review #10 - visualisation

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -10,7 +10,6 @@
 #include "geo.h"
 
 
-
 namespace Transport
 {
 	namespace Data
@@ -26,7 +25,7 @@ namespace Transport
 		struct Stop
 		{
 			std::string name;
-			Coordinates coordinates;
+			geo::Coordinates coordinates;
 			const std::set<std::string>& GetBuses() const;
 			std::set<std::string> buses = {};
 		};
@@ -35,6 +34,8 @@ namespace Transport
 		{
 			std::string name;
 			std::vector<Stop*> stops;
+			bool is_roundtrip = false;
+
 			RouteLength route_length;
 			size_t unique_stop_count;
 		};
@@ -44,12 +45,15 @@ namespace Transport
 		{
 			// Реализуйте класс самостоятельно
 		public:
-			void AddBus(const std::string& bus_name, const std::vector<std::string_view>& stops_names);
-			void AddStop(const std::string& stop_name, Coordinates coordinates); //name + shirota + dolgota
+			void AddBus(const std::string& bus_name, const std::vector<std::string_view>& stops_names, bool is_roundtrip);
+			void AddStop(const std::string& stop_name, geo::Coordinates coordinates); //name + shirota + dolgota
 			const Bus* GetBus(std::string_view bus_name) const;
 			const Stop* GetStop(std::string_view stop_name) const;
 			void AddDistBetweenStops(std::string_view stop_from, std::string_view stop_to, int length);
 			int GetDistBetweenStops(const std::string& stop_from, const std::string& stop_to) const;
+
+			const std::vector<Bus*>& GetAllBuses() const;
+			const std::vector<Stop*>& GetAllStops() const;
 
 
 		private:
@@ -59,6 +63,8 @@ namespace Transport
 			std::unordered_map <std::string_view, Bus*> buses_for_find_;
 			std::unordered_map <std::string_view, Stop*> stops_for_find_;
 
+			std::vector<Bus*>  buses_for_sort_;
+			std::vector<Stop*> stops_for_sort_;
 
 			struct PairStringHash {
 				size_t  operator() (const std::pair<std::string, std::string>& pr) const {
