8303c589d9c2ed699d834359cceeba506ac08989
EvheniCH-Ufa <Ch.Ew.Se@yandex.ru>
2025-03-02 16:59:50 +0500

    Update transport_catalogue.cpp
    Code Review #10 - visualisation

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -1,4 +1,5 @@
 #include<unordered_set>
+#include<algorithm>
 
 #include "transport_catalogue.h"
 
@@ -6,12 +7,12 @@ namespace Transport
 {
 	namespace Data
 	{
-		void TransportCatalogue::AddBus(const std::string& bus_name, const std::vector<std::string_view>& stops_names)
+		void TransportCatalogue::AddBus(const std::string& bus_name, const std::vector<std::string_view>& stops_names, bool is_roundtrip)
 		{
 			std::string name;
 			std::vector<Stop*> current_bus_stops;
 
-			Coordinates last_stop_coordinate;
+			geo::Coordinates last_stop_coordinate;
 			std::string last_stop;
 
 			bool first_stop = true;
@@ -19,20 +20,30 @@ namespace Transport
 
 			std::unordered_set<Stop*> uniq_stops;
 
-			for (auto stop_name : stops_names) // probeg po ostanovkam
+			/* probeg po ostanovkam: if round -> sam zamknetsa 
+			*  иначе количество остановок = уникальное*2-1
+			*  иначе длина маршрутов * 2 
+			*/ 
+			for (auto stop_name : stops_names) 
 			{   // find stop
 
 				auto it_find = stops_for_find_.find(stop_name);
 				if (it_find == stops_for_find_.end())
 				{
-					continue;
+//					continue;
+					return;
 				}
 				current_bus_stops.push_back(it_find->second); // add in bus_stops current stop
 
 				if (!first_stop) // calculate route len
-				{
-					route_len.geo += ComputeDistance(last_stop_coordinate, it_find->second->coordinates);
+				{   //                если не круговой, то расстояние ГЕО удваиваем 
+					route_len.geo += (is_roundtrip ? 1.0 : 2.0) * ComputeDistance(last_stop_coordinate, it_find->second->coordinates);
+
 					route_len.street += GetDistBetweenStops(last_stop, std::string(stop_name));
+					if (!is_roundtrip) // если не круговой, то ищем расстояние по улицам в обратку
+					{
+						route_len.street += GetDistBetweenStops(std::string(stop_name), last_stop);
+					}
 				}
 				first_stop = false;
 				last_stop_coordinate = it_find->second->coordinates;
@@ -42,12 +53,13 @@ namespace Transport
 			}
 
 			// create and insert new bus
-			Bus bus = { bus_name, std::move(current_bus_stops), route_len, uniq_stops.size() };
+			Bus bus = { bus_name, std::move(current_bus_stops), is_roundtrip, route_len, uniq_stops.size() };
 			buses_.push_back(std::move(bus));
 
 			// ptr for new bus in dec 
 			auto last_bus = &(buses_.back());
 			buses_for_find_.insert({ last_bus->name, last_bus });
+			buses_for_sort_.push_back(last_bus);
 
 			// for all stops this bus add ptr new bus 
 			for (auto stop : uniq_stops)
@@ -56,11 +68,12 @@ namespace Transport
 			}
 		}
 
-		void TransportCatalogue::AddStop(const std::string& stop_name, Coordinates coordinates) //name + shirota + dolgota
+		void TransportCatalogue::AddStop(const std::string& stop_name, geo::Coordinates coordinates) //name + shirota + dolgota
 		{
 			Stop stop = { stop_name, coordinates };
 			stops_.push_back(std::move(stop));
 			stops_for_find_.insert({ stops_.back().name, &(stops_.back()) }); // можжет тут быть
+			stops_for_sort_.push_back(&(stops_.back()));
 		}
 
 		const Bus* TransportCatalogue::GetBus(std::string_view bus_name) const
@@ -90,14 +103,14 @@ namespace Transport
 
 		int TransportCatalogue::GetDistBetweenStops(const std::string& stop_from, const std::string& stop_to) const
 		{
-			auto it_find = distance_between_stops_.find({stop_from, stop_to});
+			auto it_find = distance_between_stops_.find({ stop_from, stop_to });
 
 			if (it_find != distance_between_stops_.end())
 			{
 				return it_find->second;
 			}
 
-			it_find = distance_between_stops_.find({ stop_from, stop_to });
+			it_find = distance_between_stops_.find({ stop_to, stop_from });
 
 			if (it_find != distance_between_stops_.end())
 			{
@@ -105,7 +118,16 @@ namespace Transport
 			}
 
 			return 0;
-		};
+		}
+		const std::vector<Bus*>& TransportCatalogue::GetAllBuses() const
+		{
+			return buses_for_sort_;
+		}
+
+		const std::vector<Stop*>& TransportCatalogue::GetAllStops() const
+		{
+			return stops_for_sort_;// TODO: вставьте здесь оператор return
+		}
 
 		const std::set<std::string>& Stop::GetBuses() const
 		{
