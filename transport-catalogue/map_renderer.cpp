d425a679852c6ba41b2e5e67154e7867688a4db3
EvheniCH-Ufa <Ch.Ew.Se@yandex.ru>
2025-03-04 22:10:55 +0500

    Update map_renderer.cpp
    Ревью 1

--- transport-catalogue/map_renderer.cpp
+++ transport-catalogue/map_renderer.cpp
@@ -5,10 +5,11 @@
  * Визуализация маршртутов вам понадобится во второй части итогового проекта.
  */
 namespace map_render {
-    const SphereProjector MakeSphereProjector(const Transport::Data::TransportCatalogue& catalog, const RenderSettings& settings)
+
+    const SphereProjector MapRenderer::MakeSphereProjector()
     {
         std::vector<geo::Coordinates> geo_coords;
-        auto all_buses = catalog.GetAllBuses();
+        auto all_buses = catalog_.GetAllBuses();
         for (const auto& bus : all_buses)
         {
             for (const auto& stop : bus->stops)
@@ -16,9 +17,8 @@ namespace map_render {
                 geo_coords.push_back(stop->coordinates);
             }
         }
-        return SphereProjector{geo_coords.begin(), geo_coords.end(), settings.WIDTH, settings.HEIGHT, settings.PADDING };
+        return SphereProjector{ geo_coords.begin(), geo_coords.end(), settings_.WIDTH, settings_.HEIGHT, settings_.PADDING };
     }
-    
 
     std::pair<svg::Text, svg::Text> MakeBusLabel(const RenderSettings& settings, const svg::Color& color, const svg::Point& pos, const std::string& text)
     {
@@ -69,11 +69,11 @@ namespace map_render {
     }
 
 
-    void BusesRenderToDoc(svg::Document& document, const Transport::Data::TransportCatalogue& catalog, const RenderSettings& settings, const SphereProjector& zoom)
+    void MapRenderer::BusesRenderToDoc(svg::Document& document)
     {
         int color_line_index = 0;
 
-        auto all_buses(catalog.GetAllBuses());
+        auto all_buses(catalog_.GetAllBuses());
         std::sort(all_buses.begin(), all_buses.end(), [](Transport::Data::Bus* left, Transport::Data::Bus* right)
             { return left->name < right->name; });
 
@@ -89,11 +89,12 @@ namespace map_render {
 
             svg::Polyline route;
 
-            std::vector<svg::Point> point_coords;
-            point_coords.reserve(bus->stops.size() * (bus->is_roundtrip ? 1 : 2));
+            const auto [first_point, last_point] = MakeBusRoute(route, *bus);
+
+        /*    point_coords.reserve(bus->stops.size() * (bus->is_roundtrip ? 1 : 2));
             for (const auto& stop : bus->stops)
             {
-                svg::Point point = zoom(stop->coordinates);
+                svg::Point point = sphere_projector_(stop->coordinates);
                 point_coords.push_back(point);
                 route.AddPoint(point);
             }
@@ -103,24 +104,29 @@ namespace map_render {
                 {
                     route.AddPoint(point_coords[i]);
                 }
-            }
+            }*/
 
-            auto color = settings.COLOR_PALETTE[color_line_index % settings.COLOR_PALETTE.size()];
-            route.SetStrokeColor(color);
-            route.SetStrokeWidth(settings.LINE_WIDTH);
+
+
+            const auto color = settings_.COLOR_PALETTE[color_line_index % settings_.COLOR_PALETTE.size()];
+            SetBusRouteSettings(route, color);
+
+            /*route.SetStrokeColor(color);
+            route.SetStrokeWidth(settings_.LINE_WIDTH);
             route.SetStrokeLineCap(svg::StrokeLineCap::ROUND);
             route.SetStrokeLineJoin(svg::StrokeLineJoin::ROUND);
-            route.SetFillColor(svg::NoneColor);
+            route.SetFillColor(svg::NoneColor);*/
+
 
             {
-                auto [label_back, label] = MakeBusLabel(settings, color, point_coords[0], bus->name);
+                auto [label_back, label] = MakeBusLabel(settings_, color, first_point, bus->name);
                 buses_labels.push_back(std::move(label_back));
                 buses_labels.push_back(std::move(label));
             }
 
             if (!bus->is_roundtrip && bus->stops[0] != bus->stops[bus->stops.size()-1])
             {
-                auto [label_back, label] = MakeBusLabel(settings, color, point_coords[point_coords.size() - 1], bus->name);
+                auto [label_back, label] = MakeBusLabel(settings_, color, last_point, bus->name);
                 buses_labels.push_back(std::move(label_back));
                 buses_labels.push_back(std::move(label));
             }
@@ -135,9 +141,48 @@ namespace map_render {
         }
     }
 
-    void StopsRenderToDoc(svg::Document& document, const Transport::Data::TransportCatalogue& catalog, const RenderSettings& settings, const SphereProjector& zoom)
+    void MapRenderer::SetBusRouteSettings(svg::Polyline& route, const svg::Color& color)
     {
-        std::vector<Transport::Data::Stop*> all_stops(catalog.GetAllStops());
+        route.SetStrokeColor(color);
+        route.SetStrokeWidth(settings_.LINE_WIDTH);
+        route.SetStrokeLineCap(svg::StrokeLineCap::ROUND);
+        route.SetStrokeLineJoin(svg::StrokeLineJoin::ROUND);
+        route.SetFillColor(svg::NoneColor);
+    }
+
+    const std::pair<svg::Point, svg::Point> MapRenderer::MakeBusRoute(svg::Polyline& route, const Transport::Data::Bus& bus)
+    {
+        std::vector<svg::Point> point_coords;
+
+        point_coords.reserve(bus.stops.size() * (bus.is_roundtrip ? 1 : 2));
+        for (const auto& stop : bus.stops)
+        {
+            svg::Point point = sphere_projector_(stop->coordinates);
+            point_coords.push_back(point);
+            route.AddPoint(point);
+        }
+        if (!bus.is_roundtrip && point_coords.size() >= 2)
+        {
+            for (int i = point_coords.size() - 2; i >= 0; --i)
+            {
+                route.AddPoint(point_coords[i]);
+            }
+        }
+        return { point_coords[0], point_coords.back() };
+    }
+
+
+    void MapRenderer::SetStopProperty(svg::Circle& stop_place, svg::Point point)
+    {
+        stop_place.SetCenter(point);
+        stop_place.SetRadius(settings_.STOP_RADIUS);
+        stop_place.SetFillColor("white");
+    }
+
+
+    void MapRenderer::StopsRenderToDoc(svg::Document& document)
+    {
+        std::vector<Transport::Data::Stop*> all_stops(catalog_.GetAllStops());
         std::sort(all_stops.begin(), all_stops.end(), [](Transport::Data::Stop* left, Transport::Data::Stop* right)
             { return left->name < right->name; });
 
@@ -151,16 +196,17 @@ namespace map_render {
                 continue;
             }
 
+            svg::Point stop_centre = sphere_projector_(stop->coordinates);
             svg::Circle stop_place;
+            SetStopProperty(stop_place, stop_centre);
 
-            svg::Point point = zoom(stop->coordinates);
-            stop_place.SetCenter(point);
-            stop_place.SetRadius(settings.STOP_RADIUS);
-            stop_place.SetFillColor("white");            
+            /*            stop_place.SetCenter(point);
+            stop_place.SetRadius(settings_.STOP_RADIUS);
+            stop_place.SetFillColor("white");            */
            
             document.Add(stop_place);
 
-            auto [label_back, label] = MakeStopLabel(settings, point, stop->name);
+            auto [label_back, label] = MakeStopLabel(settings_, stop_centre, stop->name);
             stops_labels.push_back(std::move(label_back));
             stops_labels.push_back(std::move(label));
         }
@@ -171,10 +217,9 @@ namespace map_render {
         }
     }
 
-    void RenderToDoc(svg::Document& document, const Transport::Data::TransportCatalogue& catalog, const RenderSettings& settings)
+    void MapRenderer::RenderToSVGDoc(svg::Document& document)
     {
-        const SphereProjector zoom = MakeSphereProjector(catalog, settings);
-        BusesRenderToDoc(document, catalog, settings, zoom);
-        StopsRenderToDoc(document, catalog, settings, zoom);
+        BusesRenderToDoc(document);
+        StopsRenderToDoc(document);
     }
 } //map_render
